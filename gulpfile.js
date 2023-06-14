aded670c34b5c3906b6574376abc71bf8fa2dec1
NicolasAmato <nicolasamato@outlook.com>
2023-06-14 14:49:34 -0300

    volviendo a correguir

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,31 @@
 // optimisa las funciones de JS
 
-const { src, dest, watch, parallel,} = require('gulp');
+const { src, dest, watch, parallel} = require('gulp');
 
 //css
 const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 //imagenes
-const cache = require('gulp-cache');
-const imagemi = require('gulp-imagemi');
 const webp = require('gulp-webp');
 
 function css(done) {
     src('src/scss/**/*.scss')//identiuficar el archivo SASS
-     .pipe(plumber())
-     .pipe(sass())//compilarlo
-     .pipe(dest('build/css'));  // almacenarla en el disco duro
+    .pipe(plumber())
+    .pipe(sass())//compilarlo
+    .pipe(dest('build/css'));  // almacenarla en el disco duro
 
 
     done(); //callback que avisa a gulp cuando llegamos al final
 }
 
-function imagenes(done){
-    const opciones = {
-        optimizationLevel: 3
-    }  
-    src('src/img/**/*.{png,jpg}')
-     .pipe(cache(imagemi(opciones)))
-     .pipe(dest('build/img'))
-    done();
-
-}
 function versionWebp(done) {
     const opciones = {
         quality: 50
     };    
     src('src/img/**/*.{png,jpg}')
-     .pipe(webp())
-     .pipe(dest('build/img'))
+    .pipe(webp(opciones))
+    .pipe(dest('build/img'))
     done();
 }
 
@@ -49,6 +37,5 @@ function dev(done) {
 }
 
 exports.css = css;
-exports.imagenes= imagenes;
 exports.versionWebp = versionWebp;
-exports.dev = parallel(imagenes,versionWebp,dev);
+exports.dev = parallel(versionWebp,dev);
