24721b852fd55b11a4f74cab815a324bf40c056a
NicolasAmato <nicolasamato@outlook.com>
2023-06-21 15:46:39 -0300

    todo ok ahora

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,8 @@ const sass = require('gulp-sass')(require('sass'));
 const plumber = require('gulp-plumber');
 
 //imagenes
+const cachee = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 
 function css(done) {
@@ -18,6 +20,19 @@ function css(done) {
 
     done(); //callback que avisa a gulp cuando llegamos al final
 }
+
+function imagenes(done){
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src('src/img/**/*.{png,jpg}')
+     .pipe( cachee( imagemin(opciones) ))
+     .pipe( dest('build/img'))
+    done();
+
+}
+
+
 
 function versionWebp(done) {
     const opciones = {
@@ -37,5 +52,6 @@ function dev(done) {
 }
 
 exports.css = css;
+exports.imagenes = imagenes
 exports.versionWebp = versionWebp;
-exports.dev = parallel(versionWebp,dev);
+exports.dev = parallel( imagenes,versionWebp,dev);
