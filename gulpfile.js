2bde1f0fa1834cf62e516dcc6f9a50186923e71d
NicolasAmato <nicolasamato@outlook.com>
2023-06-26 09:44:35 -0300

    cambios ok

--- gulpfile.js
+++ gulpfile.js
@@ -10,6 +10,7 @@ const plumber = require('gulp-plumber');
 const cachee = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css(done) {
     src('src/scss/**/*.scss')//identiuficar el archivo SASS
@@ -45,6 +46,17 @@ function versionWebp(done) {
 }
 
 
+function versionavif(done) {
+    const opciones = {
+        quality: 50
+    };    
+    src('src/img/**/*.{png,jpg}')
+    .pipe(avif(opciones))
+    .pipe(dest('build/img'))
+    done();
+}
+
+
 function dev(done) {
     watch('src/scss/**/*.scss', css)
     done();
@@ -54,4 +66,5 @@ function dev(done) {
 exports.css = css;
 exports.imagenes = imagenes
 exports.versionWebp = versionWebp;
-exports.dev = parallel( imagenes,versionWebp,dev);
+exports.versionavif = versionavif;
+exports.dev = parallel( imagenes,versionWebp,versionavif,dev);
