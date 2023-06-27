94c95f5d4888ad745b81be4484fb330a87f05b83
NicolasAmato <nicolasamato@outlook.com>
2023-06-27 13:45:47 -0300

    cambios en galeria de img

--- build/js/app.js
+++ build/js/app.js
@@ -8,5 +8,14 @@ function IniciarApp(){
 
 function creargaleria(){
     const galeria = document.querySelector('galeria-fotos')
-    galeria.textContent = 'iniciando galeria'
+    for(let i = 1; i <= 12; i++){
+        const imagen = document.createElement('picture');
+        imagen.innerHTML = `
+        <source srcset="build/img/thumb/${i}.avif" type="imagen/avif">
+        <source srcset="build/img/thumb/${i}.webp" type="imagen/webp">
+        <img loading='lazy' width='200' height='300' src="build/img/thumb/${i}.jpg" alt="imagen galeria">
+        `;
+
+        galeria.appendChild(imagen);
+    }
 }
